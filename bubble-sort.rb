f61ac8dfc534006312f2cadf8a13a8f0c4700a55
macnick <nick@macexperts.gr>
2019-09-10 12:21:28 +0300

    Changed the spaces from 4 to 2

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,24 +1,24 @@
 def bubble_sort(arr)
-    n = arr.size - 1
-    for i in 1..n
-        for j in 0..n-1
-            arr[j], arr[j+1] = arr[j+1], arr[j] if arr[j] > arr[j+1]
-        end
+  n = arr.size - 1
+  for i in 1..n
+    for j in 0..n-1
+    arr[j], arr[j+1] = arr[j+1], arr[j] if arr[j] > arr[j+1]
     end
-    arr
+  end
+  arr
 end
 
 def bubble_sort_by(arr)
-    n = arr.size - 1
-    swapped = true
-    while swapped
-        swapped = false
-        n.times do |i|
-            if yield(arr[i], arr[i + 1]) > 0
-                arr[i], arr[i + 1] = arr[i + 1], arr[i]
-                swapped = true
-            end
-        end
+  n = arr.size - 1
+  swapped = true
+  while swapped
+    swapped = false
+    n.times do |i|
+      if yield(arr[i], arr[i + 1]) > 0
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
     end
-    arr
+  end
+  arr
 end
