fa0617a32643473dfd42dfb76e82b33795025a8f
Anna Myzukina <anna.muzykina83@gmail.com>
2019-09-10 13:52:18 +0300

    solve errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 def bubble_sort(arr)
   n = arr.size - 1
-  for i in 1..n
-    for j in 0..n-1
-    arr[j], arr[j+1] = arr[j+1], arr[j] if arr[j] > arr[j+1]
+  (1..n).each do |_i|
+    (0..n - 1).each do |j|
+      arr[j], arr[j + 1] = arr[j + 1], arr[j] if arr[j] > arr[j + 1]
     end
   end
   arr
@@ -14,7 +16,7 @@ def bubble_sort_by(arr)
   while swapped
     swapped = false
     n.times do |i|
-      if yield(arr[i], arr[i + 1]) > 0
+      if yield(arr[i], arr[i + 1]).positive?
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         swapped = true
       end
